56187b5dc038c776ba75ae67db4d57637f70b91d
ganatma <55006398+ganatma@users.noreply.github.com>
2022-04-25 15:52:01 -0500

    plotting, eager_lbfgs files

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
+import time
+import scipy.io
+import math
+import matplotlib.gridspec as gridspec
+from plotting import newfig
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras import layers, activations
+from scipy.interpolate import griddata
+from eager_lbfgs import lbfgs, Struct
+from pyDOE import lhs
